b9437004b47016d79485d646c0efbbeaa4544003
SrijaniSom <81528176+SrijaniSom@users.noreply.github.com>
2021-09-11 12:24:02 -0700

    Made changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import text from './data'
+import './styles.css'
+const App = () => {
+  const [Data, setData] = React.useState([]);
+  const [Count, setCount] = React.useState(0);
+  const getInputValue=(event) => {
 
-function App() {
+    const userVal=event.target.value;
+    const userValue=parseInt(userVal);
+    console.log(userValue);
+    if(userValue<0)
+    {
+      setCount(1);
+    }
+    else if(userValue>8)
+    {
+      setCount(8);
+    }
+    else
+    setCount(userValue);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h2 className="heading"> TIRED OF BORING LOREM IPSUM? </h2>
+      <section className="features">
+        <div className="sub">
+          Paragraphs:
+        </div>
+
+        <div className="num">
+          <input type="number" className="numinput" onChange={getInputValue}></input>
+        </div>
+
+        <button className="btn" onClick={()=>setData(text.slice(0,Count))}>
+          GENERATE
+        </button>
+      </section>
+    
+    <section className="body">
+      {Data.map((currElem,currIndex)=>{
+        return(
+          <div className="Paras">
+          {currElem}
+        </div>
+
+               )
+         
+      })}
+    </section>
+
+
+    
+    </>
+  )
 }
 
-export default App;
+export default App
